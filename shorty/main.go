a5fb5059680e8725547dff5449f40e75eea78d59
Tomasz Hermanowicz <tomasz.hermanowicz@dxc.com>
2024-03-19 20:56:03 +0100

    fixing spacing problem

--- shorty/main.go
+++ shorty/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println(`Hello, world!
-		On multiple lines.
-		I hope you will have a great DAY.`)
+On multiple lines.
+I hope you will have a great DAY.`)
 	fmt.Println(time.Now().UTC())
 }
